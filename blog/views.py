4df2a653ef9906fc4cb358002e28be265a372b69
kny26023 <kny26023@sookmyung.ac.kr>
2021-07-07 20:34:40 +0900

    CBV 적용

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 # Create your views here.
 
+class PostList(ListView):   #ListView를 쓸 땐 뒤에 _list를 붙여줘야!
+    model= Post
+    #template_name= 'blog/index.html'
+    ordering= '-pk'
+
+'''
 def index(request):
     posts= Post.objects.all().order_by('-pk')   #역순은 -
 
@@ -12,7 +19,13 @@ def index(request):
             'posts':posts
         }
     )
+'''
 
+class PostDetail(DetailView):
+    model= Post
+    #template_name= 'blog/single_post_page.html'
+
+'''
 def single_post_page(request, pk):
     post= Post.objects.get(pk= pk)
 
@@ -23,3 +36,4 @@ def single_post_page(request, pk):
             'post':post
         }
     )
+'''
