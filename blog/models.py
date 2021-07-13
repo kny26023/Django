68bb62659ae1c1374dc1ea36d7288b1331c65a93
kny26023 <kny26023@sookmyung.ac.kr>
2021-07-13 18:07:19 +0900

    File name

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import os
 
 # Create your models here.
 # 여기에 data table 만들기
@@ -16,3 +17,11 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
+
+    #파일 이름을 가져오는 함수
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    #파일 확장자를 가져오는 함수
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
