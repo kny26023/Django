0a1c9e3b5d4bcf98f243630c95f9a42e50937750
kny26023 <kny26023@sookmyung.ac.kr>
2021-07-15 15:44:46 +0900

    Add Author

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import os
 
 # Create your models here.
@@ -7,13 +8,17 @@ import os
 class Post(models.Model):
     title= models.CharField(max_length=30)
     content= models.TextField()
+
     head_image= models.ImageField(upload_to= 'blog/images/%Y/%m/%d/', blank= True)
     file_upload= models.FileField(upload_to= 'blog/files/%Y/%m/%d/', blank= True)
+
     created_at= models.DateTimeField(auto_now_add=True)
     updated_at= models.DateTimeField(auto_now=True)
+
+    author= models.ForeignKey(User, on_delete=models.CASCADE)   #ForeignKey-> DB 용어
     
     def __str__(self):
-        return f'[{self.pk}]    {self.title}'   #pk(primary key) -> id
+        return f'[{self.pk}]    {self.title} :: {self.author}'   #pk(primary key) -> id
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
--- singlepages/models.py
+++ singlepages/models.py
@@ -2,3 +2,4 @@ from django.db import models
 
 # Create your models here.
 # 여기에 data table 만들기
+
